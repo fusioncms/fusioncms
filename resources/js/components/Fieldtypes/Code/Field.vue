1f5cb002d5466dcae1f01be6709f6a4d0cd3b69d
Robert Harvey <robert@efellemedia.com>
2019-07-24 16:02:39 -0700

    Initialize brace on textarea

--- resources/js/components/Fieldtypes/Code/Field.vue
+++ resources/js/components/Fieldtypes/Code/Field.vue
@@ -1,17 +1,23 @@
 <template>
     <div>
-        <p-textarea
+        <!-- <p-textarea
             :name="field.handle"
             :label="field.name"
             :help="field.help"
             :placeholder="field.settings.placeholder"
             :value="value"
             @input="$emit('input', $event)"
-        ></p-textarea>
+        ></p-textarea> -->
+
+        <textarea :ref="braceClass + '_textarea'" :name="field.handle" :id="field.handle" :value="value" v-show="false"></textarea>
+
+        <div :ref="braceClass" style="width: 100%" :id="braceClass + '_id'"></div>
     </div>
 </template>
 
 <script>
+    import ace from 'brace'
+    
     export default {
         name: 'code-fieldtype',
 
@@ -26,6 +32,31 @@
                 default: '',
             },
         },
+
+        data() {
+          return {
+            editor: Object
+          }
+        },
+
+        computed: {
+          braceClass() {
+            return this.field.handle + '-brace'
+          }
+        },
+
+        mounted() {
+          require(`brace/mode/javascript`)
+          require(`brace/theme/dracula`)
+
+          this.editor = ace.edit(this.$refs[this.braceClass])
+          this.editor.setOptions({
+                 showPrintMargin: false,
+                 minLines: 5,
+                 maxLines: Infinity
+            })
+          this.editor.setTheme(`ace/theme/dracula`)
+        }
     }
 </script>
 
