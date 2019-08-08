9693927b1156d5136a8bb1186a6d98312a8ef657
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2019-08-07 17:22:18 -0700

    Move filemanager to file manager route

--- resources/js/views/FileManager/Index.vue
+++ resources/js/views/FileManager/Index.vue
@@ -1,79 +1,31 @@
 <template>
     <div>
-        <portal to="title">Files</portal>
+        <portal to="title">
+            <app-title icon="images">File Manager</app-title>
+        </portal>
 
-        <div class="bg-white p-4">
-            <div class="row">
-                <div class="col md:w-1/2 lg:w-1/4 xl:w-1/5">
-                    <div class="mb-4">
-                        <p-button @click="goToRoot">Root</p-button>
-                    </div>
-
-                    <p-treeview
-                        v-model="activeDirectory"
-                        :items="sidebarDirectories"
-                        @input="$refs.fileManager.goToDirectory(activeDirectory)"
-                    ></p-treeview>
-                </div>
-                <div class="col md:w-1/2 lg:w-3/4 xl:w-4/5">
-                    <file-manager ref="fileManager"></file-manager>
-                </div>
-            </div>
-        </div>
+        <file-manager :files="files" :directories="directories"></file-manager>
     </div>
 </template>
 
 <script>
-import FileManager from '../../components/FileManager'
-
-export default {
-    components: {
-        FileManager
-    },
-
-    data() {
-        return {
-            activeDirectory: null,
-            sidebarDirectories: [],
-
-            createDirectoryModal: false,
-            newDirectoryName: null,
-        }
-    },
-
-    mounted() {
-        this.getSidebarDirectories()
-        this.$refs.fileManager.getData()
-
-        this.$bus.$on('refreshSidebarDirectories', this.getSidebarDirectories)
-    },
-
-    methods: {
-        getSidebarDirectories() {
-            axios.get('/api/directories?recursive=true').then(({data}) => {
-                this.sidebarDirectories = data.data
-            })
+    export default {
+        data() {
+            return {
+                files: [],
+                directories: [],
+            }
         },
-
-        goToRoot() {
-            this.activeDirectory = null
-
-            this.$refs.fileManager.goToRootDirectory()
+        beforeRouteEnter(to, from, next) {
+            axios.all([
+                axios.get('/api/files'),
+                axios.get('/api/directories'),
+            ]).then(axios.spread(function (files, directories) {
+                next(function(vm) {
+                    vm.files = files.data.data
+                    vm.directories = directories.data.data
+                })
+            }))
         },
-    },
-}
+    }
 </script>
-
-<style scoped>
-.thumbnail {
-    width: 30px;
-    height: 30px;
-    background-position: center center;
-    background-size: cover;
-}
-
-.font-awesome-icon-container {
-    width: 30px;
-    height: 30px;
-}
-</style>
--- resources/js/views/Playground.vue
+++ resources/js/views/Playground.vue
@@ -1,31 +1,7 @@
 <template>
     <div>
         <portal to="title">
-            <app-title icon="images">File Manager</app-title>
+            <app-title icon="robot">Playground</app-title>
         </portal>
-
-        <file-manager :files="files" :directories="directories"></file-manager>
     </div>
 </template>
-
-<script>
-    export default {
-        data() {
-            return {
-                files: [],
-                directories: [],
-            }
-        },
-        beforeRouteEnter(to, from, next) {
-            axios.all([
-                axios.get('/api/files'),
-                axios.get('/api/directories'),
-            ]).then(axios.spread(function (files, directories) {
-                next(function(vm) {
-                    vm.files = files.data.data
-                    vm.directories = directories.data.data
-                })
-            }))
-        },
-    }
-</script>
