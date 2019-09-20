f28c0a8fd2798d6916d8887d33a4a51bc539c455
Robert Harvey <robert@efellemedia.com>
2019-09-20 08:56:20 -0700

    Finalize field. Simplify array of countries.

---
+++ resources/js/components/Fieldtypes/Country/Field.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <p-select
+            :name="field.handle"
+            :label="field.name"
+            :help="field.help"
+            :value="value"
+            @input="$emit('input', $event)"
+            :options="countries"
+            placeholder="Select a country..."
+        ></p-select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'us_state-fieldtype',
+
+        props: {
+            field: {
+                type: Object,
+                required: true,
+            },
+
+            value: {
+                required: false,
+                default: null,
+            },
+        },
+
+        computed: {
+            countries() {
+                return Object.values(this.field.type.data)
+            },
+        },
+
+        created() {
+            let index = _.findIndex(this.countries, function(country) {
+                return country.checked == true
+            })
+
+            if (index !== -1 && ! this.countries) {
+                this.$emit('input', this.countries[index].value)
+            }
+        }
+    }
+</script>
+
