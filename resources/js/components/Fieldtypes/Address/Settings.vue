7222b696ee211fb146916dbc30c8d2d4e486bf1f
Robert <robert@efellemedia.com>
2019-09-20 12:15:03 -0700

    Address Fieldtype (#48)
    * Build address form, display and save address values
    
    * Add google map with geolocation and lat/long finder
    
    * Add settings for maps API key
    
    * Map and input styles, cleanup

---
+++ resources/js/components/Fieldtypes/Address/Settings.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p-input name="settings.api_key"
+        label="API Key"
+        help='You will need to generate a <a href="https://developers.google.com/maps/documentation/javascript/get-api-key" target="_blank">Google Maps API key</a> in order to view the map component and retrieve latitude and longitude coordinates for your address'
+        placeholder=""
+        v-model="value.api_key">
+      
+    </p-input>
+  </div>
+</template>
+
+<script>
+    import fieldtype from '../../../mixins/fieldtype'
+
+    export default {
+        name: 'address-fieldtype-settings',
+
+        mixins: [fieldtype],
+
+        props: {
+            value: Object
+        },
+
+        data() {
+          return {
+
+          }
+        }
+    }
+</script>
+
