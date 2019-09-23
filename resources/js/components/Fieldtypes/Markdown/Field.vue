0a229b9e9519167cb6f3a4d57031f4f7fb26ef0c
Robert Harvey <robert@efellemedia.com>
2019-09-23 15:56:58 -0700

    Add preview toggle, clean up styles

--- resources/js/components/Fieldtypes/Markdown/Field.vue
+++ resources/js/components/Fieldtypes/Markdown/Field.vue
@@ -1,18 +1,23 @@
 <template>
-    <div class="row">
-        <div class="col" :class="[previewOpen ? 'w-1/2' : 'w-100']">
+    <div class="row markdown">
+        <div class="col" :class="[previewOpen ? 'w-1/2' : 'w-full']">
             <p-textarea
                 :name="field.handle"
                 :label="field.name"
                 :help="field.help"
                 :value="value"
                 @input="$emit('input', $event)"
+                :rows="8"
             ></p-textarea>
         </div>
-        <div class="w-1/2" v-if="previewOpen">
-            <div class="card h-full">
+        <div class="w-1/2 flex flex-col" v-if="previewOpen">
+            <div class="form__label">{{field.name}} Preview</div>
+            <div class="markdown__preview card overflow-auto">
                 <div class="p-2" v-html="markdown"></div>
             </div>
+        </div>
+        <div class="w-full col">
+            <a href="#" class="button button--primary button--small mt-3 mb-4" @click="previewOpen = !previewOpen">Toggle Preview</a>
         </div>
     </div>
 </template>
@@ -43,7 +48,7 @@
 
         computed: {
             markdown() {
-                return marked(this.value)
+                return this.value ? marked(this.value) : ''
             }
         }
     }
