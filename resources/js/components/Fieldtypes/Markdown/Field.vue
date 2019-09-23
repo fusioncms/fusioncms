2ce13009d13dd365f726502bc04ecdcac1b1b0f8
Robert Harvey <robert@efellemedia.com>
2019-09-23 15:28:12 -0700

    Add live preview window

--- resources/js/components/Fieldtypes/Markdown/Field.vue
+++ resources/js/components/Fieldtypes/Markdown/Field.vue
@@ -1,18 +1,25 @@
 <template>
-    <div>
-        <p-textarea
-            :name="field.handle"
-            :label="field.name"
-            :help="field.help"
-            :value="value"
-            @input="$emit('input', $event)"
-        ></p-textarea>
+    <div class="row">
+        <div class="col" :class="[previewOpen ? 'w-1/2' : 'w-100']">
+            <p-textarea
+                :name="field.handle"
+                :label="field.name"
+                :help="field.help"
+                :value="value"
+                @input="$emit('input', $event)"
+            ></p-textarea>
+        </div>
+        <div class="w-1/2" v-if="previewOpen">
+            <div class="card h-full">
+                <div class="p-2" v-html="markdown"></div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import marked from 'marked'
-    
+
     export default {
         name: 'markdown-fieldtype',
 
@@ -27,6 +34,18 @@
                 default: '',
             },
         },
+
+        data() {
+            return {
+                previewOpen: true
+            }
+        },
+
+        computed: {
+            markdown() {
+                return marked(this.value)
+            }
+        }
     }
 </script>
 
