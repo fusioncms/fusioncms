d7e633059a20ce50bdbeaa1a6433654063cdfcdf
Robert Harvey <robert@efellemedia.com>
2019-11-12 12:07:29 -0800

    Add debug text

--- resources/js/mixins/forms.js
+++ resources/js/mixins/forms.js
@@ -4,14 +4,8 @@ import ConfirmModal from '../components/Modals/ConfirmModal'
 export default {
   beforeRouteLeave(to, from, next) {
     if (store.state.form.preventNavigation) {
-      console.log('Navigation prevented')
-
       store.commit('form/setConfirmModalVisible', true)
       store.commit('form/setConfirmModalCallback', next)
-
-      // if (confirm('You have unsaved changes! Are you sure you want to leave? All changes will be discarded.')) {
-      //   next()
-      // }
     } else {
       next()
     }
