7be44d48d4aab9ca0599c720effd85adf4d6ff1c
Robert Harvey <robert@efellemedia.com>
2019-11-12 12:04:16 -0800

    Add new confirmation modal to show and control navigation flow

--- resources/js/mixins/forms.js
+++ resources/js/mixins/forms.js
@@ -1,12 +1,17 @@
 import store from '../vuex'
+import ConfirmModal from '../components/Modals/ConfirmModal'
 
 export default {
   beforeRouteLeave(to, from, next) {
     if (store.state.form.preventNavigation) {
       console.log('Navigation prevented')
-      if (confirm('You have unsaved changes! Are you sure you want to leave? All changes will be discarded.')) {
-        next()
-      }
+
+      store.commit('form/setConfirmModalVisible', true)
+      store.commit('form/setConfirmModalCallback', next)
+
+      // if (confirm('You have unsaved changes! Are you sure you want to leave? All changes will be discarded.')) {
+      //   next()
+      // }
     } else {
       next()
     }
