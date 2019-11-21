91b8d1da6b05c979720c3d1045150e1e8d6da26c
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2019-11-21 12:14:40 -0800

    Add dispatcher unit tests

---
+++ tests/Feature/APIDispatcherTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the FusionCMS application.
+ *
+ * (c) efelle creative <appdev@efelle.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Feature;
+
+use Tests\Foundation\TestCase;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class APIDispatcherTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function as_an_authorized_user_the_dispatcher_should_resolve_requests_appropriately()
+    {
+        $this->actingAs($this->admin, 'api');
+
+        $response = fusion()->get('users/1');
+
+        $this->assertNotNull($response);
+        $this->assertEquals($response->data->name, 'Jane Doe');
+    }
+
+    /** @test */
+    public function as_an_unauthorized_guest_the_dispatcher_should_resolve_requests_when_explicitly_authorized()
+    {
+        $response = fusion()->authorize()->get('users/1');
+
+        $this->assertNotNull($response);
+        $this->assertEquals($response->data->name, 'Jane Doe');
+    }
+}
