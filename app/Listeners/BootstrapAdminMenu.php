47361b59b5ceda92d5fa1249d46b4a9f3f17d57a
Robert Harvey <robert@efellemedia.com>
2019-12-04 16:02:13 -0800

    Re-add log viewer interface

--- app/Listeners/BootstrapAdminMenu.php
+++ app/Listeners/BootstrapAdminMenu.php
@@ -129,6 +129,11 @@ class BootstrapAdminMenu
                 'to'   => '/settings',
                 'icon' => 'cog',
             ]);
+            
+            $menu->add('Logs')->data([
+                'to'   => '/logs',
+                'icon' => 'bug'
+            ]);
 
             if (app()->isLocal()) {
                 $menu->add('Telescope')->data([
