c1b3725e959460f218544c559e3e22f1945c2fc1
Shane Krolikowski <skrolikowski@gmail.com>
2019-12-30 22:25:48 +0000

    sync settings

--- app/Providers/SettingsServiceProvider.php
+++ app/Providers/SettingsServiceProvider.php
@@ -34,8 +34,10 @@ class SettingsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (! File::exists(settings_path()) or File::lastModified(config_path('settings.php')) > File::lastModified(settings_path())) {
-            $this->initializeSettingsFile();
+        if (! File::exists(settings_path())) {
+            $this->createSettingsFile();
+        } elseif (File::lastModified(config_path('settings.php')) > File::lastModified(settings_path())) {
+            $this->updateSettingsFile();
         }
 
         $this->app->singleton('settings', function ($app) {
@@ -45,7 +47,12 @@ class SettingsServiceProvider extends ServiceProvider
         });
     }
 
-    protected function initializeSettingsFile()
+    /**
+     * Initialize `settings.json` file.
+     * 
+     * @return [type] [description]
+     */
+    protected function createSettingsFile()
     {
         $settings = collect(config('settings.settings'))
             ->groupBy('section')
@@ -54,6 +61,35 @@ class SettingsServiceProvider extends ServiceProvider
 
                 $items->each(function ($item) use (&$settings) {
                     $settings[$item['handle']] = $item['default'] ?? null;
+                });
+
+                return $settings;
+            })
+            ->toArray();
+
+        File::put(settings_path(), json_encode($settings, JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Sync existing `settings.json` file with `config/settings.php`
+     * 
+     * @return void
+     */
+    protected function updateSettingsFile()
+    {
+        $existing = json_decode(file_get_contents(settings_path()), true);
+
+        $settings = collect(config('settings.settings'))
+            ->groupBy('section')
+            ->map(function ($items, $section) use ($existing) {
+                $settings = [];
+
+                $items->each(function ($item) use (&$settings, $section, $existing) {
+                    if (isset($existing[$section][$item['handle']])) {
+                        $settings[$item['handle']] = $existing[$section][$item['handle']];
+                    } else {
+                        $settings[$item['handle']] = $item['default'] ?? null;
+                    }
                 });
 
                 return $settings;
