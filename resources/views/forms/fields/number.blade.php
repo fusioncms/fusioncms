ffa3e06bf83424e5fbe8167bfd820857e1e32a99
Robert Harvey <robert@efellemedia.com>
2019-12-30 14:58:16 -0800

    Add default form fields for divider, email, input, number, textarea

---
+++ resources/views/forms/fields/number.blade.php
@@ -0,0 +1,13 @@
+<div class="mb-6">
+    <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="{{ $field->handle }}">
+        {{ $field->name }}
+    </label>
+    
+    <input type="number" 
+      name="{{ $field->handle }}" 
+      id="{{ $field->handle }}" 
+      class="form__control w-full"
+      min="{{ $field->settings['min'] }}"
+      max="{{ $field->settings['max'] }}"
+      step="{{ $field->settings['steps'] }}">
+</div>
