098caedb16bd4844abc1da277620361ad7d87dc7
Robert Harvey <robert@efellemedia.com>
2019-12-31 09:15:15 -0800

    Re-add default index file

--- themes/Hello/resources/views/index.blade.php
+++ themes/Hello/resources/views/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.default')
 
 @section('content')
-    @form('contact_us')
+    @if (theme('setting.say_hello'))
+        <div class="text-center items-center justify-center flex flex-col flex-1">
+            <h1 class="text-6xl font-thin tracking-wider leading-tight">{{ theme('setting.greeting') ?? 'Welcome to FusionCMS' }}</h1>
+
+            <p class="tracking-wide">{{ theme('setting.message') ?? 'We think you\'ll enjoy your time here' }}</p>
+        </div>
+    @endif
 @endsection
