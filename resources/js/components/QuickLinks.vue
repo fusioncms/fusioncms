46b32341f9ec7bc85c2dfa5cde7ad5348f7bf3a1
Robert <robert@efellemedia.com>
2020-01-17 12:27:38 -0800

    Datetime field UI restructure (#290)
    * Change default icon for matrix
    
    * Replace pencil icon on edit buttons
    
    * Restructure design of datetime field
    
    * Remove unused styles
    
    * Disallow clicks on calendar icon

--- resources/js/components/QuickLinks.vue
+++ resources/js/components/QuickLinks.vue
@@ -5,7 +5,7 @@
                 <div class="flex flex-grow-0 flex-shrink-0 items-center justify-center w-28 mr-2 h-full border-r border-dashed border-gray-200">
                     <span class="fa-layers fa-fw fa-4x flex-shrink-0">
                         <fa-icon :icon="['fas', 'circle']" class="fa-fw text-primary-500"></fa-icon>
-                        <fa-icon :icon="['far', (link.icon || 'pencil')]" class="fa-fw fa-inverse" transform="shrink-8"></fa-icon>
+                        <fa-icon :icon="['far', (link.icon || 'pencil-alt')]" class="fa-fw fa-inverse" transform="shrink-8"></fa-icon>
                     </span>
                 </div>                
 
