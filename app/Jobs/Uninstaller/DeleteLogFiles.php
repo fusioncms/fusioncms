30c9825f90852cd4f99e755f1d6479c7ab4cc40d
Shane Krolikowski <skrolikowski@gmail.com>
2020-03-18 17:52:29 +0000

    additional commands to clean cms

---
+++ app/Jobs/Uninstaller/DeleteLogFiles.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the FusionCMS application.
+ *
+ * (c) efelle creative <appdev@efelle.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Jobs\Uninstaller;
+
+class DeleteLogFiles
+{
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        exec('rm ' . storage_path('logs/*.log'), $output);
+    }
+}
