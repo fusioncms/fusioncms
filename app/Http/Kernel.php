1b7e983fa8580b21c4f4a3186eb309820bba51f0
Shane Krolikowski <skrolikowski@gmail.com>
2020-06-16 16:46:01 -0700

    Removing passport references (#571)
    * removing passport references
    
    * updates

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,9 +36,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-
-            // \Fusion\Http\Middleware\SiteVisibility::class,
-            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
         ],
 
         'api' => [
--- config/auth.php
+++ config/auth.php
@@ -42,8 +42,9 @@ return [
         ],
 
         'api' => [
-            'driver'   => 'passport',
+            'driver'   => 'token',
             'provider' => 'users',
+            'hash'     => false,
         ],
     ],
 
@@ -101,7 +102,21 @@ return [
             'email'    => 'auth.emails.password',
             'table'    => 'password_resets',
             'expire'   => 60,
+            'throttle' => 60,
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Confirmation Timeout
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the amount of seconds before a password confirmation
+    | times out and the user is prompted to re-enter their password via the
+    | confirmation screen. By default, the timeout lasts for three hours.
+    |
+    */
+
+    'password_timeout' => 10800,
 
 ];
